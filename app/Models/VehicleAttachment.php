7279e609ea2fb9d0081fe737e260e77bfbec9515
Hasan-Raza-InterSys <hasanintersysltd@gmail.com>
2025-08-05 15:19:21 +0500

    fileurl

--- app/Models/VehicleAttachment.php
+++ app/Models/VehicleAttachment.php
@@ -4,15 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
 
 class VehicleAttachment extends Model
 {
     use HasFactory;
 
     protected $fillable = ['vehicle_ad_id', 'file_path'];
+    protected $appends = ['file_url'];
 
     public function vehicleAd()
     {
         return $this->belongsTo(VehicleAd::class);
+    }
+    
+    public function getFileUrlAttribute()
+    {
+         return Storage::disk('public')->url($this->file_path);
     }
 }

d55feef0defb15fe0bcffc4e6e8d7c504ec5912c
Syed Ali Raza Zaidi <syed.ali2@intersyslimited.org>
2025-10-21 19:03:42 +0500

    updated master

--- server.cjs
+++ server.cjs
@@ -14,8 +14,8 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-// const LARAVEL_API_URL = "https://modmarket.retrocubedev.com";
-const LARAVEL_API_URL = "http://127.0.0.1:8000";
+const LARAVEL_API_URL = "https://modmarket.retrocubedev.com";
+// const LARAVEL_API_URL = "http://127.0.0.1:8000";
 
 let onlineUsers = new Map();
 
